fc30a173a6f2c544e3765a6402d2e8a9b061e1d9
Anthony Podkowa <tpodkow2@illinois.edu>
2019-08-01 15:03:30 -0500

    uigetfilepath: Updated documentation

--- uigetfilepath.m
+++ uigetfilepath.m
@@ -1,5 +1,8 @@
 function filepath = uigetfilepath(varargin)
 % UIGETFILEPATH Returns the full filepath to file selected with UI.
+% filepath = uigetfilepath(FILTERSPEC, TITLE)
+% See: uigetfile
     [fn,fp] = uigetfile(varargin{:});
     filepath = [fp fn];
+    
 end

3cc9521c41fe8400d48db00cc7e69d45caccb8f5
Anthony Podkowa <tpodkow2@illinois.edu>
2020-03-02 09:47:39 -0600

    Pulled jobMail from Master

---
+++ jobMail.m
@@ -0,0 +1,18 @@
+function jobMail(subject,msg)
+% JOBMAIL Send an email at end of job
+% function jobMail(subject,msg)
+
+%    email = 'tpodkow2@illinois.edu';
+    email = '8159544873@vtext.com';
+   if ~exist('subject','var')
+      subject = 'Matlab Job Done';
+   end
+   if ~exist('msg','var')
+      msg = 'Job ended at ';
+      msg = [msg datestr(now)];
+   end
+
+   cmd = sprintf('echo ''%s'' | mail -s ''%s'' %s',msg, subject, email);
+   system(cmd);
+
+end

987dd9726fbeaf91cc99b57e39994abc084eaf43
VerasonicsMaster <vantage@notanemail.com>
2022-08-03 12:36:46 -0700

    rf2timetag: Fixed 2 dim input bug
    With 2 dim input, the output size was scalar instead of vector valued, causing a reshape crash.  Appended 1 to make it vector-valued.

--- rf2timetag.m
+++ rf2timetag.m
@@ -6,5 +6,9 @@ function timeStamp = rf2timetag(rf)
     tt = double(rf(1:2,:));
     tt(tt<0)  = tt(tt<0)+2^16;
     timeStamp = [1, 2^16]*tt*25e-6;
-    timeStamp = reshape(timeStamp,size(rf,2:ndims(rf)));
+    outSize = size(rf,2:ndims(rf));
+    if length(outSize)==1
+        outSize(end+1) = 1;
+    end
+    timeStamp = reshape(timeStamp,outSize);
 end
